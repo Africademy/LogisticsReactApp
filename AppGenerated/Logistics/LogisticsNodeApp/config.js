e390e52bb5e27e30740700509fd0a9a8f1bb658d
Bhairesh <bhairesh@arithaconsulting.com>
2021-03-31 11:45:14 +0530

    Update: Server DB config added.

--- AppGenerated/Logistics/LogisticsNodeApp/config.js
+++ AppGenerated/Logistics/LogisticsNodeApp/config.js
@@ -1,5 +1,15 @@
+const DB = {
+  localhost: {
+    url: "mongodb://localhost:27017/LogisticsDb",
+    key: "231sad_ItCanBeAnyRandString_UGHASD82371923192J"
+  },
+  server: {
+    url: "mongodb://13.233.146.100:4542/logistics",
+    key: "231sad_ItCanBeAnyRandString_UGHASD82371923192J"
+  }
+}
 var config = {
-  DB_CONNECTION: "mongodb://localhost:27017/LogisticsDb",
-  TOKKEN_SECRET: "231sad_ItCanBeAnyRandString_UGHASD82371923192J"
+  DB_CONNECTION: DB.server.url,
+  TOKKEN_SECRET: DB.server.key
 };
 module.exports = config;
